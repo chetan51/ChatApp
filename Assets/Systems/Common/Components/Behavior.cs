577a0828fed7e9b5ff3d39042c376d797e2be151
Chetan Surpur <chetan51@gmail.com>
2021-03-23 12:37:49 -0700

    Started on DataManager

---
+++ Assets/Systems/Common/Components/Behavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class Behavior : MonoBehaviour {
+    #region Logging
+    protected void DebugLog(params object[] text) {
+        Debug.Log($"[{GetType().Name}] {string.Join(" ", text)}");
+    }
+
+    protected void DebugLogError(params object[] text) {
+        Debug.LogError($"[{GetType().Name}] {string.Join(" ", text)}");
+    }
+    #endregion
+
+    #region MonoBehaviour
+    protected virtual void OnValidate() {
+        GetComponentOnValidate();
+    }
+    #endregion
+
+    #region GetComponent
+    void GetComponentOnValidate() {
+        IEnumerable<FieldInfo> fieldsToGet = this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
+            .Where(f => f.FieldType.IsSubclassOf(typeof(Component)))
+            .Where(f => Attribute.IsDefined(f, typeof(GetComponentOnValidate)));
+
+        foreach (FieldInfo field in fieldsToGet) {
+            if (field.IsPrivate && !Attribute.IsDefined(field, typeof(SerializeField))) continue;
+            if (field.GetValue(this) != null) continue;
+            field.SetValue(this, this.GetComponent(field.FieldType));
+        }
+    }
+    #endregion
+}
+
+class GetComponentOnValidate : Attribute {
+    public GetComponentOnValidate() { }
+}
---
+++ Assets/Systems/Data/Components/DataManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChatApp.Data {
+
+    public class DataManager : Singleton<DataManager>
+    {
+        public const string pathToData = "sample_data";
+            
+        private ConversationsData conversationsData;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            TextAsset dataAsset = Resources.Load<TextAsset>(pathToData);
+            conversationsData = JsonUtility.FromJson<ConversationsData>(dataAsset.text);
+        }
+    }
+
+    [Serializable]
+    public class PersonData
+    {
+        public string firstName;
+        public string lastName;
+        public string pictureURL;
+    }
+
+    [Serializable]
+    public class ConversationData
+    {
+        public PersonData person;
+    }
+
+    [Serializable]
+    public class ConversationsData
+    {
+        public List<ConversationData> conversations;
+    }
+
+}
